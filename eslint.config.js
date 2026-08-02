b9221f682bfa9a1e4719d697092d164a04040052
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-08-02 14:47:37 +0300

    add DTO and body parser middleware

--- eslint.config.js
+++ eslint.config.js
@@ -5,26 +5,27 @@ import { defineConfig } from 'eslint/config';
 import eslintPluginPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
-  },
-  tseslint.configs.recommended,
-  {
-    files: ['**/*.{ts,mts,cts}'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-    },
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        onUnsupportedTypeScriptVersion: 'warn',
-      },
-    },
-  },
-  eslintPluginPrettier,
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		plugins: { js },
+		extends: ['js/recommended'],
+		languageOptions: { globals: globals.node },
+	},
+	tseslint.configs.recommended,
+	{
+		files: ['**/*.{ts,mts,cts}'],
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'warn',
+			'@typescript-eslint/no-unused-vars': 'warn',
+			'@typescript-eslint/no-empty-object-type': 'off',
+		},
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				onUnsupportedTypeScriptVersion: 'warn',
+			},
+		},
+	},
+	eslintPluginPrettier,
 ]);
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -5,6 +5,8 @@ import type { ILogger } from '../logger/logger.interface.js';
 import type { NextFunction, Request, Response } from 'express';
 import { types } from '../types.js';
 import type { IUserController } from './user.controller.interface.js';
+import type { UserLoginDto } from './DTO/user-login.dto.js';
+import type { UserRegisterDto } from './DTO/user-register.dto.js';
 
 injectable();
 export class UserController extends BaseController implements IUserController {
@@ -24,12 +26,13 @@ export class UserController extends BaseController implements IUserController {
 		]);
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
 		next(new HttpError(401, 'Authorization error', 'Login'));
 		// this.ok(res, 'Login');
 	}
 
-	register(req: Request, res: Response, next: NextFunction): void {
+	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
 		this.ok(res, 'Register');
 	}
 }
