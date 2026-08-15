09941e3158ec6932b172f159ce6e50d38b475699
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-08-15 13:10:47 +0300

    replace prisma adapter

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@database/client.js';
-import { PrismaLibSql } from '@prisma/adapter-libsql';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { inject, injectable } from 'inversify';
 import type { UserModel } from '@database/models.js';
 import type { ILogger } from '../logger/logger.interface.js';
@@ -11,7 +11,7 @@ export class PrismaService {
 	dd: UserModel;
 
 	constructor(@inject(types.ILogger) protected logger: ILogger) {
-		const adapter = new PrismaLibSql({ url: 'file:./dev.db' });
+		const adapter = new PrismaBetterSqlite3({ url: 'file:./dev.db' });
 		this.client = new PrismaClient({ adapter });
 	}
 
