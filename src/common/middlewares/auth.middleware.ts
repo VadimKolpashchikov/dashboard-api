ef4d5012d3d30e1fbe33faf45a1091eafc881dd0
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-08-16 22:10:19 +0300

    up auth middleware

--- src/common/middlewares/auth.middleware.ts
+++ src/common/middlewares/auth.middleware.ts
@@ -1,6 +1,7 @@
 import type { Request, Response, NextFunction } from 'express';
 import type { IMiddleware } from '../types/middleware.interface.js';
 import jwt from 'jsonwebtoken';
+import { HttpError } from '../../errors/http-error.class.js';
 
 export class AuthMiddleware implements IMiddleware {
 	constructor(private readonly secretKey: string) {}
@@ -14,7 +15,7 @@ export class AuthMiddleware implements IMiddleware {
 			if (token) {
 				jwt.verify(token, this.secretKey, (err, payload) => {
 					if (err) {
-						next();
+						next(new HttpError(403, 'Authorization error - invalid token', 'AuthMiddleware'));
 					} else if (payload && typeof payload === 'object') {
 						req.user = payload.email;
 						next();
